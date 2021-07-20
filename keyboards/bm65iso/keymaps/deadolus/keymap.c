8fa01c21afdbbb6588836546a2b2e16cf28755e1
Simon Egli <4662005+Deadolus@users.noreply.github.com>
2021-07-20 14:31:54 +0200

    Add Wheel tapdance configuration instead of pgup/pgdown
    Page Up/Down will now scroll up/down on single press.
    Double press will tapdance page up/down.

--- keyboards/bm65iso/keymaps/deadolus/keymap.c
+++ keyboards/bm65iso/keymaps/deadolus/keymap.c
@@ -19,12 +19,16 @@
 #ifdef TAP_DANCE_ENABLE
 enum {
   TD_HOME_END,
+  TD_WHEEL_UP,
+  TD_WHEEL_DOWN,
 };
 
 // Tap Dance definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
   //tap once for home, twice for end
-  [TD_HOME_END] = ACTION_TAP_DANCE_DOUBLE(KC_HOME, KC_END)
+  [TD_HOME_END] = ACTION_TAP_DANCE_DOUBLE(KC_HOME, KC_END),
+  [TD_WHEEL_UP] = ACTION_TAP_DANCE_DOUBLE(KC_MS_WH_UP,KC_PGUP),
+  [TD_WHEEL_DOWN] = ACTION_TAP_DANCE_DOUBLE(KC_MS_WH_DOWN,KC_PGDOWN)
 };
 #endif
 
@@ -68,8 +72,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BL] = LAYOUT(
       KC_CAPS,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,    KC_7,    KC_8,    KC_9,   KC_0,   KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_ENT, TD(TD_HOME_END),
-      KC_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS,         KC_PGUP,
-      KC_LSHIFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSHIFT, KC_UP,   KC_PGDN,
+      KC_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS,         TD(TD_WHEEL_UP),
+      KC_LSHIFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSHIFT, KC_UP,  TD(TD_WHEEL_DOWN),
       KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, MO(_RED_),         KC_LEFT, KC_DOWN, KC_RGHT
 
       ),
