8d389b5f62cb6319c9c234d6364fe855f2a39711
Simon Egli <4662005+Deadolus@users.noreply.github.com>
2021-07-28 22:14:19 +0200

    Adding tab double dance

--- keyboards/bm65iso/keymaps/deadolus/keymap.c
+++ keyboards/bm65iso/keymaps/deadolus/keymap.c
@@ -27,23 +27,51 @@ enum  layers {
   _YELLOW_
 };
 
-void dance_cln_finished(qk_tap_dance_state_t *state, void *user_data) {
+void dance_blue_finished(qk_tap_dance_state_t *state, void *user_data) {
+  if(get_mods()) return;
   if (state->count == 1) {
         if (state->interrupted || !state->pressed) {
-        register_code16(KC_ESC);
-        unregister_code16(KC_ESC);
+        register_code16(KC_TAB);
+        unregister_code16(KC_TAB);
         return;
         }
-        layer_on(_GREEN_);
+        layer_on(_BLUE_);
     } else {
-        layer_on(_GREEN_);
+      if(state->count == 2) {
+        //if (state->interrupted) {
+        layer_on(_BLUE_);
+      //} else if (state->pressed) return TD_DOUBLE_HOLD;
+      }
     }
+}
+void dance_cln_finished(qk_tap_dance_state_t *state, void *user_data) {
+  if(get_mods()) return;
+  if (state->count == 1) {
+    if (state->interrupted || !state->pressed) {
+      register_code16(KC_ESC);
+      unregister_code16(KC_ESC);
+      return;
+    }
+    layer_on(_GREEN_);
+  } else {
+    if(state->count == 2) {
+      //if (state->interrupted) {
+      layer_on(_GREEN_);
+      //} else if (state->pressed) return TD_DOUBLE_HOLD;
+    }
+    /* else if(state->count == 3) { */
+    /*   //if (state->interrupted) { */
+    /*   layer_on(_BLUE_); */
+    /*   //} else if (state->pressed) return TD_DOUBLE_HOLD; */
+    /* } */
+  }
 }
 
 void dance_cln_reset(qk_tap_dance_state_t *state, void *user_data) {
-    if (state->count == 1 ) {
-        layer_off(_GREEN_);
-    }
+  if (state->count == 1 ) {
+    layer_off(_GREEN_);
+    layer_off(_BLUE_);
+  }
 }
 
 #ifdef TAP_DANCE_ENABLE
@@ -52,6 +80,7 @@ enum {
   TD_WHEEL_UP,
   TD_WHEEL_DOWN,
   TD_ESC_GREEN,
+  TD_TAB_BLUE
 };
 
 // Tap Dance definitions
@@ -60,7 +89,8 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_HOME_END] = ACTION_TAP_DANCE_DOUBLE(KC_HOME, KC_END),
   [TD_WHEEL_UP] = ACTION_TAP_DANCE_DOUBLE(KC_MS_WH_UP,KC_PGUP),
   [TD_WHEEL_DOWN] = ACTION_TAP_DANCE_DOUBLE(KC_MS_WH_DOWN,KC_PGDOWN),
-  [TD_ESC_GREEN] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_cln_finished, dance_cln_reset)
+  [TD_ESC_GREEN] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_cln_finished, dance_cln_reset),
+  [TD_TAB_BLUE] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_blue_finished, dance_cln_reset)
 };
 #endif
 
@@ -89,7 +119,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /*     ), */
   [_BL] = LAYOUT(
       KC_CAPS,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,    KC_7,    KC_8,    KC_9,   KC_0,   KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
-      LT(_BLUE_,KC_TAB),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, LT(_YELLOW_, KC_ENT), TD(TD_HOME_END),
+      TD(TD_TAB_BLUE),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, LT(_YELLOW_, KC_ENT), TD(TD_HOME_END),
       TD(TD_ESC_GREEN), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS,         TD(TD_WHEEL_UP),
       KC_LSHIFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSHIFT, KC_UP,  TD(TD_WHEEL_DOWN),
       KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, MO(_RED_),         KC_LEFT, KC_DOWN, KC_RGHT
@@ -250,17 +280,17 @@ void setCapsLock(void) {
 }
 
 void lightUpKey(int key, int layer) {
-      //this key does something - light it up
-      HSV hsv = {
-        .h = pgm_read_byte(&ledmap[layer][0]),
-        .s = pgm_read_byte(&ledmap[layer][1]),
-        .v = pgm_read_byte(&ledmap[layer][2]),
-      };
+  //this key does something - light it up
+  HSV hsv = {
+    .h = pgm_read_byte(&ledmap[layer][0]),
+    .s = pgm_read_byte(&ledmap[layer][1]),
+    .v = pgm_read_byte(&ledmap[layer][2]),
+  };
 
-      RGB rgb = hsv_to_rgb(hsv);
-      float f = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
-      if( (layer != 0) || (rgb_matrix_get_flags() != LED_FLAG_ALL))
-        rgb_matrix_set_color(key, f * rgb.r, f * rgb.g, f * rgb.b);
+  RGB rgb = hsv_to_rgb(hsv);
+  float f = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
+  if( (layer != 0) || (rgb_matrix_get_flags() != LED_FLAG_ALL))
+    rgb_matrix_set_color(key, f * rgb.r, f * rgb.g, f * rgb.b);
 }
 
 void set_layer_color(int layer) {
